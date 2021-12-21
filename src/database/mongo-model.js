220e2a956905fd3895d50562c564e94092dd6917
Mariano <mariano_farace@hotmail.com>
2021-12-21 09:06:54 +0100

    enforce code Style

--- src/database/mongo-model.js
+++ src/database/mongo-model.js
@@ -1,14 +1,16 @@
-
 const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
 const UserSchema = new Schema({
-  username: { type: String,  required: true },
-  tasks:[{taskName: String, start_date: { type: Date, default: Date.now, required: true }, due_date: { type: Date, required: true } } ]
-  
-});
+  username: { type: String, required: true },
+  tasks: [{
+    taskName: String,
+    start_date: { type: Date, default: Date.now, required: true },
+    due_date: { type: Date, required: true },
+  }],
 
+});
 
 // Compile model from schema
 const User = mongoose.model('User', UserSchema);
