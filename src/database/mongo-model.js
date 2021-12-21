bc6b6a11ce855ab09db029a7d6436aed1d8a6057
Mariano <mariano_farace@hotmail.com>
2021-12-21 08:57:49 +0100

    Define MongoDB model

---
+++ src/database/mongo-model.js
@@ -0,0 +1,15 @@
+
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+  username: { type: String,  required: true },
+  tasks:[{taskName: String, start_date: { type: Date, default: Date.now, required: true }, due_date: { type: Date, required: true } } ]
+  
+});
+
+
+// Compile model from schema
+const User = mongoose.model('User', UserSchema);
+module.exports = User;
